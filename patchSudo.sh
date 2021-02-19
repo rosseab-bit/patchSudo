7c27c201d0c7ea03e239ba08485eb55cc6614eae
Ricardo <72170523+rosseab-bit@users.noreply.github.com>
2021-02-19 09:33:03 -0300

    Update patchSudo.sh
    install patch

--- patchSudo.sh
+++ patchSudo.sh
@@ -1,18 +1,19 @@
 #/bin/bash
 echo "get file from url : https://www.sudo.ws/dist/sudo-1.9.5p2.tar.gz"
+sleep 5
 wget https://www.sudo.ws/dist/sudo-1.9.5p2.tar.gz
 echo "for more information visit: https://www.sudo.ws/alerts/unescape_overflow.html"
 echo "--------------------------------------------------------------------"
 
 echo "extracting files => sudo-1.9.5p2.tar.gz"
 gzip -d sudo-1.9.5p2.tar.gz
-sleep 2
+sleep 5
 echo "extracting files => sudo-1.9.5p2.tar"
 tar -xvf sudo-1.9.5p2.tar
-sleep 2
+sleep 5
 
-echo "starting patch Linux SUDO !"
-cd sudo-1.9.5p2 && ./configure; make; make install
-
+echo "start patch Linux SUDO !"
+echo "cd sudo-1.9.5p2 && ./configure; make; make install"
+sleep 5
 echo "ThankYou :)"
 
